7cc717f8b8e0c4e9aa179a96e49a1ccc676485cd
dartnow21 <83269930+dartnow21@users.noreply.github.com>
2022-09-05 16:29:13 +0300

    Update 014.sql

--- 014.sql
+++ 014.sql
@@ -1,4 +1,4 @@
-SELECT Ships.class, name, country
-FROM Ships INNER JOIN Classes
-ON Classes.class = Ships.class
+SELECT s.class, s.name, c.country
+FROM Ships s INNER JOIN Classes c
+ON c.class = s.class
 WHERE numGuns >= 10
