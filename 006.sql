90db615263d00e8b53b2826851a8a001865def22
dartnow21 <83269930+dartnow21@users.noreply.github.com>
2022-09-11 14:40:27 +0300

    Update 006.sql

--- 006.sql
+++ 006.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT maker, speed
-FROM Product p JOIN Laptop l
+SELECT DISTINCT p.maker, l.speed
+FROM Product AS p JOIN Laptop AS l
 ON p.model = l.model
 WHERE type = 'Laptop' AND hd >= 10
