becf804b74070ede96d11bd89ff407dc3a984d53
dartnow21 <83269930+dartnow21@users.noreply.github.com>
2022-09-11 14:41:40 +0300

    Update 007.sql

--- 007.sql
+++ 007.sql
@@ -1,14 +1,14 @@
-SELECT l.model, price
+SELECT l.model, l.price
 FROM Laptop l JOIN Product p
 ON l.model = p.model
 WHERE p.maker = 'B'
 UNION 
-SELECT PC.model, price
+SELECT PC.model, l.price
 FROM PC JOIN Product p
 ON PC.model = p.model
 WHERE p.maker = 'B'
 UNION 
-SELECT pr.model, price
+SELECT pr.model, l.price
 FROM Printer pr JOIN Product p
 ON pr.model = p.model
 WHERE p.maker = 'B'
